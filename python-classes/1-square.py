8317d6728c504fec410c8ae7e57a8f7c40774106
Yann11GAB <danieloubassou@gmail.com>
2025-01-29 01:38:18 +0100

    first project

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 class Square:
+    """
+    This class defines a square.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new Square instance.
+        
+        Args:
+            size: The size of the square (no type/value verification)
+        """
         self.__size = size
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 class Square:
+    """
+    This class defines a square.
+    """
+
     def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
         
+        Args:
+            size (int): The size of the square. Defaults to 0.
+        
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
