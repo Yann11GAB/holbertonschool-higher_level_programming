c68c1ba27d1364e959dc25f3cb047da9da99b47a
Yann11GAB <danieloubassou@gmail.com>
2025-03-06 10:19:01 +0100

    first commit

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+SELECT score, name
+-- container  
+FROM second_table
+-- ordered 
+ORDER BY score DESC;
